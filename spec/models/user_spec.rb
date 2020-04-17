158fb9cff25caa234c46a3bb26a3231128ddb39d
braulio <jonathan_b2j@hotmail.com>
2020-04-16 20:24:29 -0500

    create posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'user creation' do
+    before do
+      @user = User.create(email:'test@mail.com', password: '123456', 
+        password_confirmation: '123456', first_name: 'braulio', last_name: 'segura') 
+    end
+    it 'can be created' do
+      expect(@user).to be_valid
+    end
+
+    it 'can not be created with last and first name' do
+        @user.first_name = nil
+        @user.last_name = nil
+        expect(@user).to_not be_valid
+    end
+  end
+
 end
